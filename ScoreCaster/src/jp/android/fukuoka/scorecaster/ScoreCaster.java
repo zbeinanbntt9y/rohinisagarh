89fa26f81be13046f5a33551ad93658b410b8ba9
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-05-09 01:43:38 +0800

    初期インポート

---
+++ ScoreCaster/src/jp/android/fukuoka/scorecaster/ScoreCaster.java
@@ -0,0 +1,13 @@
+package jp.android.fukuoka.scorecaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoreCaster extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
