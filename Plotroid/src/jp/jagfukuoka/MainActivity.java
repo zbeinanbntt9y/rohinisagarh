4732d2b39f215ed7247aafbdd4f520e44fe78a79
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-09-01 21:56:55 +0800

    first commit

--- Plotroid/src/jp/jagfukuoka/MainActivity.java
+++ Plotroid/src/jp/jagfukuoka/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.jagfukuoka;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+                
+        final Button button = (Button) findViewById(R.id.ok);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// ダイアログの表示
+            	AlertDialog.Builder dlg;
+            	dlg = new AlertDialog.Builder(MainActivity.this);
+            	dlg.setTitle("TEST");
+            	dlg.setMessage("Hello, World!");
+            	dlg.setPositiveButton("OK", null);
+            	dlg.show();
+            }
+        });
     }
 }
