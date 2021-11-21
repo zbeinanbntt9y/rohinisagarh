70a493d1e102594e07656a39720e04d1bb97de16
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-11-21 08:08:27 +0800

    ファイル名変更

--- GPStoMail/src/jp/android/fukuoka/gpstomail/Main.java
+++ GPStoMail/src/jp/android/fukuoka/gpstomail/Main.java
@@ -3,7 +3,7 @@ package jp.android.fukuoka.gpstomail;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class mail extends Activity {
+public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
