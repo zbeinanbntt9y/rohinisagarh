a149c087af875092d69343f5e3dc982a04c13079
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-11-25 05:09:45 +0800

    音が出た

--- EightBit/src/jp/jagfukuoka/eightbit/main.java
+++ EightBit/src/jp/jagfukuoka/eightbit/main.java
@@ -1,5 +1,6 @@
 package jp.jagfukuoka.eightbit;
 
+import jp.jagfukuoka.eightbit.Sound.*;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,10 +10,14 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        this.audio = new AudioMachine();
+        Sound sound = new Sin();
+        sound.createAudio(20,400);
+        this.audio.Play(sound);
     }
-    
+    private AudioMachine audio;
     @Override
     public void onStop(){
-    	
+    	this.audio.Stop();
     }
 }
