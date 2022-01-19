5b6d41e7a60d02cef516ce1c4331430d57c8dd84
zbeinanbntt9y <wanghongjun0104@yeah.net>
2022-01-19 12:35:34 +0800

    テスト用にListボタン追加

--- sodefuri/src/jp/jagfukuoka/sodefuri/MainActivity.java
+++ sodefuri/src/jp/jagfukuoka/sodefuri/MainActivity.java
@@ -16,9 +16,12 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -32,7 +35,7 @@ import android.widget.Toast;
  * @author shikajiro
  * 
  */
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity{
 
 	private static final String REQUEST_URL = "http://sodefuri.appspot.com/register";
 
@@ -47,18 +50,28 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.main);
 
 		// -----[ボタンの設定]
-		Button button1 = (Button) findViewById(R.id.Button01);
-		button1.setOnClickListener(this);
-	}
-
-	// クリックイベント
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.Button01:
-			checkBluetooth();
-			startActivity(new Intent(this,RecentListViewActivity.class));
-			break;
-		}
+		Button button1 = (Button) findViewById(R.id.OKButton);
+		button1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				switch (v.getId()) {
+				case R.id.OKButton:
+					ProgressDialog.show(MainActivity.this, null, "登録中...", true);
+					checkBluetooth();
+					startActivity(new Intent(MainActivity.this,
+							RecentListViewActivity.class));
+					break;
+				}
+			}
+		});
+		
+		Button listButton = (Button) findViewById(R.id.ListButton);
+		listButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this,RecentListViewActivity.class));
+			}
+		});
 	}
 
 	/**
@@ -71,10 +84,10 @@ public class MainActivity extends Activity implements OnClickListener {
 			// Bluetoothはサポートされてません
 			return;
 		}
-		
+
 		if (mBluetoothAdapter.isEnabled()) {
 			// -----[利用可能なので次の処理]
-			String mac_address = getBTAddress();
+			String mac_address = mBluetoothAdapter.getAddress();
 			sendJson(mac_address);
 		} else {
 			// -----[利用不可なので許可アラート表示]
@@ -96,50 +109,17 @@ public class MainActivity extends Activity implements OnClickListener {
 	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_ENABLE_BT) {
-			if (resultCode == RESULT_OK) {
-				// Bluetoothが利用可能になりました
-				String mac_address = getBTAddress();
-				sendJson(mac_address);
-			} else if (resultCode == RESULT_CANCELED) {
-				// Bluetoothは利用不可です
-			}
-		} else if (requestCode == REQUEST_STATE_CHANGE_BT) {
-			switch (resultCode) {
-			case BluetoothAdapter.STATE_TURNING_ON:
-			case BluetoothAdapter.STATE_ON:
-			case BluetoothAdapter.STATE_TURNING_OFF:
-			case BluetoothAdapter.STATE_OFF:
-				break;
-			}
-		}
-	}
-
-	/**
-	 * BlueToothのmacアドレスを取得する
-	 * 
-	 * @author _simo
-	 * @return
-	 */
-	private String getBTAddress() {
-		switch (mBluetoothAdapter.getScanMode()) {
-		case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
-		case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
-		case BluetoothAdapter.SCAN_MODE_NONE:
-			break;
+		if (requestCode != REQUEST_ENABLE_BT) {
+			return;
 		}
 
-		switch (mBluetoothAdapter.getState()) {
-		case BluetoothAdapter.STATE_OFF:
-		case BluetoothAdapter.STATE_ON:
-		case BluetoothAdapter.STATE_TURNING_OFF:
-		case BluetoothAdapter.STATE_TURNING_ON:
-			break;
+		if (resultCode == RESULT_OK) {
+			// Bluetoothが利用可能になりました
+			String mac_address = mBluetoothAdapter.getAddress();
+			sendJson(mac_address);
+		} else if (resultCode == RESULT_CANCELED) {
+			// Bluetoothは利用不可です
 		}
-
-		// mBluetoothAdapter.getName();
-		// mBluetoothAdapter.getAddress();
-		return mBluetoothAdapter.getAddress();
 	}
 
 	/**
@@ -148,7 +128,8 @@ public class MainActivity extends Activity implements OnClickListener {
 	 * @author _simo
 	 * @param address
 	 */
-	private void sendJson(String address) {
+	private void sendJson(final String address) {
+
 		// -----[クライアント設定]
 		HttpClient httpclient = new DefaultHttpClient();
 		HttpPost httppost = new HttpPost(REQUEST_URL);
@@ -171,9 +152,11 @@ public class MainActivity extends Activity implements OnClickListener {
 
 			// -----[サーバーからの応答を取得]
 			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-				Toast.makeText(this, "登録しました", Toast.LENGTH_LONG).show();
+				Toast.makeText(MainActivity.this, "登録しました", Toast.LENGTH_LONG)
+						.show();
 			} else {
-				Toast.makeText(this, "[error]: " + response.getStatusLine(),
+				Toast.makeText(MainActivity.this,
+						"[error]: " + response.getStatusLine(),
 						Toast.LENGTH_LONG).show();
 			}
 		} catch (UnsupportedEncodingException e) {
@@ -181,5 +164,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
