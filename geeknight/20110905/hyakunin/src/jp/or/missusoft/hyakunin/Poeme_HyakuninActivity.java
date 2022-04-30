6a0e1019c8d68fd695a807a335d852e46886710e
zbeinanbntt9y <wanghongjun0104@yeah.net>
2022-05-01 00:42:37 +0800

    音声再生TTSをつけてみました。日本語音声のTTSが必要です。

--- geeknight/20110905/hyakunin/src/jp/or/missusoft/hyakunin/Poeme_HyakuninActivity.java
+++ geeknight/20110905/hyakunin/src/jp/or/missusoft/hyakunin/Poeme_HyakuninActivity.java
@@ -2,25 +2,37 @@ package jp.or.missusoft.hyakunin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 //import java.io.IOException;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Poeme_HyakuninActivity extends Activity {
+public class Poeme_HyakuninActivity extends Activity implements OnClickListener,OnInitListener {
 	
 //	private EditText ed_poeme;
 	private CharDrawViewMultiLine ed_poeme;
 
 	private CharDrawView ed_writer;
 //	private VTextView ed_writer;
+	private Button Speech_btn;
+	
+	private TextToSpeech tts;
 	private int MaxCount = 0;
 	private int CurCount = 0;
 	
 	private String[] poeme ;
 	private String[] writer;
+	private String[] poemeToRead;
 	
 	private boolean Wait_flg = false;
+	static String tag ="Poeme_HyakuninActivity";
 	
 	
     /** Called when the activity is first created. */
@@ -34,6 +46,7 @@ public class Poeme_HyakuninActivity extends Activity {
 
         poeme = getResources().getStringArray(R.array.Poemes);
         writer = getResources().getStringArray(R.array.writers);
+        poemeToRead=getResources().getStringArray(R.array.PoemesToRead);
         // ècèëÇ´ÉrÉÖÅ[ÉèÇ…ê›íËÇ∑ÇÈ
 
         ed_poeme.setText(poeme[0].replace(" ", Character.toString((char) 0d)));
@@ -41,6 +54,16 @@ public class Poeme_HyakuninActivity extends Activity {
         MaxCount = poeme.length;
         CurCount = 0;
         Wait_flg = true;
+        
+        Speech_btn =(Button) findViewById(R.id.buttonSpeech);
+        Speech_btn.setOnClickListener(this);
+        
+        tts = new TextToSpeech(getApplicationContext(), this);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tts.shutdown();
     }
     
     @Override
@@ -74,5 +97,28 @@ public class Poeme_HyakuninActivity extends Activity {
     	}
 		return Value;
     }
+    
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+        	Locale locale = Locale.JAPANESE;
+            if (tts.isLanguageAvailable(locale) >= TextToSpeech.LANG_AVAILABLE) {
+                tts.setLanguage(locale);
+                tts.speak("ê›íËäÆóπ", TextToSpeech.QUEUE_FLUSH, null);
+            } else {
+				Log.e(tag, "Error SetLocale");
+                tts.speak("I'm not ready!", TextToSpeech.QUEUE_FLUSH, null);
+            }
+            
+        } else {
+        	Log.e(tag,"Oops!");
+        }
+    }
+    
+    @Override
+    public void onClick(final View v) {
+        tts.speak(poemeToRead[CurCount].toString(), TextToSpeech.QUEUE_FLUSH, null);
+    }
+        
     
 }
